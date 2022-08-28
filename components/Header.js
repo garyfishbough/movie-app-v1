f7043f7ef53fd870bff5fcfc9fc910ac0973d8ce
elfish <41020376+garyfishbough@users.noreply.github.com>
2022-08-28 16:43:22 -0700

    styled the header section

--- components/Header.js
+++ components/Header.js
@@ -5,14 +5,17 @@ import { HomeIcon, UserIcon, PhoneIcon, InformationCircleIcon } from '@heroicons
 
 const Header = () => {
     return (
-        <div>
-            <div className="">
+        <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+            {/* Header Icons Imported structure from HeaderIcon.js */}
+            <div className="flex">
                 <HeaderIcon Icon={HomeIcon} title="HOME" />
                 <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
                 <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
                 <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
             </div>
+            {/* IMDb image load from outside source */}
             <Image
+                className="cursor-pointer active:brightness-110"
                 src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
                 width={100}
                 height={100}
