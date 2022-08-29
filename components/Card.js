813873a718efbaf62adc88ce972ad12ad6be5c6b
elfish <41020376+garyfishbough@users.noreply.github.com>
2022-08-28 21:15:07 -0700

    styled the card component, made it responsive using grid

--- components/Card.js
+++ components/Card.js
@@ -2,22 +2,23 @@
 import Image from 'next/image';
 import { HandThumbUpIcon } from '@heroicons/react/24/outline';
 
+// Called all the information needed for the card layout
 const Card = ({ result }) => {
     const BASE_URL = 'https://image.tmdb.org/t/p/original';
     return (
-        <div>
+        <div className="p-3 hover:text-white cursor-pointer active:text-red-400 xl:hover:scale-105 transition-transform duration-200">
             <Image
                 layout="responsive"
                 src={BASE_URL + result.backdrop_path}
                 width={200}
                 height={100}
             />
-            <div className="truncate">
-                <p>{result.overview}</p>
-                <h2>{result.title || result.name}</h2>
-                <p>
+            <div className="p-2">
+                <p className="truncate text-lg">{result.overview}</p>
+                <h2 className="text-lg font-bold">{result.title || result.name}</h2>
+                <p className="flex items-center ">
                     {result.release_date || result.first_air_date}{' '}
-                    <HandThumbUpIcon className="h-5" />
+                    <HandThumbUpIcon className="h-5 ml-3 mr-1" />
                     {result.vote_count}
                 </p>
             </div>
